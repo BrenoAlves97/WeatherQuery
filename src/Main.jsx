6f01707b6043aa559b26577a109d0d897dc009cd
Breno Alves <breno_ja@hotmail.com>
2023-05-06 08:55:40 -0300

    add archivo font and config global styles

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./App.jsx";
 
+// styles
+import "./global-style/global.scss";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
