d5b03c3f51921b806910c51ae82981aa5d441a86
Breno Alves <breno_ja@hotmail.com>
2023-05-06 08:45:38 -0300

    config firebase file, export auth/authentication

---
+++ src/firebase/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBc05jU0uvrBRrEDCJHIGhiDk14l1l6DSM",
+  authDomain: "project01-4b835.firebaseapp.com",
+  projectId: "project01-4b835",
+  storageBucket: "project01-4b835.appspot.com",
+  messagingSenderId: "480483398644",
+  appId: "1:480483398644:web:680b954630b54df3bbdfa1",
+  measurementId: "G-T588D0L2EB",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+export { auth };
